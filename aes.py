7f4de9f04dbc6928ec3b8c309032e1a4a9cdd790
kota <52403688+yashikota@users.noreply.github.com>
2022-06-27 16:56:36 +0900

    aes 鍵変更

--- aes.py
+++ aes.py
@@ -1,12 +1,10 @@
 from Crypto.Cipher import AES
 
 input_text = input("暗号化したい文字列を入力してください : ")
-
 text = input_text.encode("utf-8")
-nonce = bytes.fromhex("0011223344556677")
 
-key = bytes.fromhex("00112233445566778899aabbccddeeff")
-# print("key = " + str(key))
+nonce = bytes.fromhex("1111111111111111") # 16桁
+key = bytes.fromhex("11111111111111111111111111111111") # 32桁
 
 encipher = AES.new(key, AES.MODE_CTR, nonce=nonce)
 
