054b33c590725ad0a6b7cfae2e913b535db63944
kota <52403688+yashikota@users.noreply.github.com>
2022-08-08 05:37:12 +0900

    update: コメントの変更

--- rsa.py
+++ rsa.py
@@ -8,6 +8,7 @@ class RSA:
         self.n = 0
 
     def is_prime(self, n):
+        """素数の判定"""
         if n == 2:
             return True
         if n < 2 or n % 2 == 0:
@@ -18,6 +19,7 @@ class RSA:
         return True
 
     def input_p(self):
+        """素数pの入力"""
         # 素数でなければ再入力
         while not self.is_prime(self.p):
             self.p = int(input("pを入力して下さい : "))
@@ -25,6 +27,7 @@ class RSA:
                 print("pは素数でなければなりません")
 
     def input_q(self):
+        """素数qの入力"""
         # 素数でなければ再入力
         while not self.is_prime(self.q):
             self.q = int(input("qを入力して下さい : "))
@@ -32,15 +35,17 @@ class RSA:
                 print("qは素数でなければなりません")
 
     def calc_n(self):
+        """pとqを乗算してnを求める"""
         self.n = self.p * self.q
         print("n = " + str(self.n))
 
     def calc_r(self):
+        """p-1とq-1を乗算してrを求める"""
         self.r = (self.p - 1) * (self.q - 1)
         print("r = " + str(self.r))
 
-    # self.pとself.qの互いに素な自然数を求める
     def calc_e(self):
+        """pとqの互いに素な自然数eを求める"""
         # self.rと最大公約数が1の数字を求める
         for i in range(2, self.r):
             if self.gcd(i, self.r) == 1:
@@ -49,20 +54,23 @@ class RSA:
         print("e = " + str(self.e))
 
     def gcd(self, a, b):
+        """ユークリッド互除法"""
         while b != 0:
             a, b = b, a % b
         return a
 
-    # edを(p-1)(q-1)で割った余りがあまり1となる自然数dを求める
     def calc_d(self):
+        """edを(p-1)(q-1)で割った余りがあまり1となる自然数dを求める"""
         while(self.d * self.e % self.r != 1):
             self.d += 1
         print("d = " + str(self.d))
 
     def encrypt(self, m):
+        """メッセージを暗号化する"""
         return pow(m, self.e, self.n)
 
     def decrypt(self, c):
+        """メッセージを復元する"""
         return pow(c, self.d) % self.n
 
     def main(self):
@@ -72,11 +80,11 @@ class RSA:
         self.calc_r()
         self.calc_e()
         self.calc_d()
-        m = int(input("mを入力して下さい({}以下の自然数) : ".format(self.n)))
+        m = int(input(f"mを入力して下さい({self.n}以下の自然数) : "))
         c = self.encrypt(m)
         print("c = " + str(c))
-        m_ = self.decrypt(c)
-        print("m = " + str(m_))
+        m2 = self.decrypt(c)
+        print("m = " + str(m2))
 
 
 if __name__ == "__main__":
